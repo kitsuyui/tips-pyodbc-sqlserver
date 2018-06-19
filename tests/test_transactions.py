c60c6155ac8ea0734584ce2d32102cc7c3225ffa
kitsuy <kitsuy@eac.ne.jp>
2018-06-19 10:05:05 +0900

    Add case of autocommit and snapshot isolation level

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -98,6 +98,33 @@ def test_transaction_isolation_level_snapshot_2():
     conn_b.close()
 
 
+def test_transaction_isolation_level_snapshot_3():
+    conn_a = pyodbc.connect(get_connection_string())
+
+    # set conn_b to transaction isolation level snapshot
+    conn_b = pyodbc.connect(get_connection_string())
+    conn_b.autocommit = True
+    conn_b.execute('SET TRANSACTION ISOLATION LEVEL SNAPSHOT')
+
+    # Once result is decided
+    results = conn_b.execute('SELECT * FROM x')
+
+    # Even if the content has changed
+    eq_(tuple(results), ())
+    conn_a.execute('INSERT INTO x(a) VALUES (1)')
+    conn_a.commit()
+
+    # then returns the old result set
+    (result,) , = conn_b.execute('SELECT * FROM x')
+    eq_(result, 1)
+
+    # teardown
+    conn_a.execute('DELETE FROM x')
+    conn_a.commit()
+    conn_a.close()
+    conn_b.close()
+
+
 def test_transaction_isolation_level_dirty_1():
     conn_a = pyodbc.connect(get_connection_string())
 
